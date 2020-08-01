068aeba7bb0ead4d47a2b7e3a7fa18bcd43c308b
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2020-08-01 17:07:57 +0800

    First commit - mostly gatsbyjs default recipe

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Renzo Bongocan" />
+    <h1>Renzo Bongocan</h1>
+    <h2>Software Developer.</h2>
+    <p>High-tech, low-life. Coffee and OCR on my free time.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link> <br />
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+  </Layout>
+)
+
+export default IndexPage
