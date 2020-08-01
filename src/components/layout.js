7594f499ebdbbce968008991b5ee24914f3244dc
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2020-08-01 17:42:12 +0800

    Remove boilerplate elements, crudely alter styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,20 +25,26 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
       <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
+      style={{
+        display: `flex`,
+        alignItems: `center`,
+        justifyContent: `center`,
+        textAlign: `middle`,
+        height: `100vh`,
+      }}
+        // style={{
+        //   margin: `0 auto`,
+        //   maxWidth: 960,
+        //   padding: `0 1.0875rem 1.45rem`,
+        // }}
       >
         <main>{children}</main>
-        <footer>
+        {/* <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        </footer> */}
       </div>
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,11 +11,6 @@ const IndexPage = () => (
     <h1>Renzo Bongocan</h1>
     <h2>Software Developer.</h2>
     <p>High-tech, low-life. Coffee and OCR on my free time.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
