6da33bce9edf535689a009c3ad898b19141d5658
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2020-08-03 12:56:42 +0800

    Use an edgy 404 message

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>The sky above the port was the color of television, tuned to a dead channel.</p>
   </Layout>
 )
 
